b261fc1b4845bfe6fae91d97c8b6604f6acc8f5f
Yuki Kodama <endflow.net@gmail.com>
2021-03-08 01:27:25 +0900

    Communicate via channel

--- toykio-server/src/main.rs
+++ toykio-server/src/main.rs
@@ -1,22 +1,43 @@
+use std::convert::Infallible;
 use std::time::{Duration, Instant};
 
-use tokio::sync::oneshot;
+use tokio::sync::{mpsc, oneshot};
 use toykio_runtime::{Delay, Toykio};
 use warp::Filter;
 
+type Responder<T> = oneshot::Sender<T>;
+
+pub struct Req {
+    pub resp: Responder<usize>,
+}
+
+pub async fn on_request(tx: mpsc::Sender<Req>) -> Result<impl warp::Reply, Infallible> {
+    let (res_tx, res_rx) = oneshot::channel();
+    let _ = tx.send(Req { resp: res_tx }).await;
+    match res_rx.await {
+        Ok(count) => {
+            println!("count = {}", count);
+            Ok(format!("count = {}", count))
+        }
+        Err(_) => {
+            println!("ERROR: failed to receive response");
+            Ok("ERROR: failed to receive response".into())
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() {
-    let (tx, rx) = oneshot::channel();
-
-    tokio::spawn(async move {
-        match rx.await {
-            Ok(count) => println!("count = {}", count),
-            Err(_) => println!("ERROR: sender dropped"),
-        }
-    });
+    let (tx, mut rx) = mpsc::channel::<Req>(32);
 
     tokio::spawn(async move {
         let mut rt = Toykio::new();
+
+        tokio::spawn(async move {
+            while let Some(req) = rx.recv().await {
+                let _ = req.resp.send(10);
+            }
+        });
 
         rt.spawn(async {
             println!("Spawned");
@@ -28,13 +49,12 @@ async fn main() {
             assert_eq!(out, "done");
         });
 
-        if let Err(_) = tx.send(rt.num_of_running_tasks()) {
-            println!("ERROR: receiver dropped");
-        }
-
         rt.run();
     });
 
-    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
-    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+    let handler = warp::path!("toykio")
+        .and(warp::any().map(move || tx.clone()))
+        .and_then(on_request);
+
+    warp::serve(handler).run(([127, 0, 0, 1], 3030)).await;
 }
