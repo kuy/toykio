dad7e9787d73a21eee77101399383517b4b4c6fc
Yuki Kodama <endflow.net@gmail.com>
2021-03-07 23:43:56 +0900

    Use same logic with cli

--- toykio-server/src/main.rs
+++ toykio-server/src/main.rs
@@ -1,9 +1,26 @@
+use std::time::{Duration, Instant};
+
+use toykio_runtime::{Delay, Toykio};
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+    tokio::spawn(async {
+        let mut rt = Toykio::new();
 
+        rt.spawn(async {
+            println!("Spawned");
+            let when = Instant::now() + Duration::from_millis(1500);
+            let future = Delay { when };
+            println!("Wait 1.5sec...");
+            let out = future.await;
+            println!("Done");
+            assert_eq!(out, "done");
+        });
+
+        rt.run();
+    });
+
+    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
     warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
 }
